bfee763eee30cb7e75ec62e75392529204c6d031
Rob King <rob@mash.is>
2016-06-07 15:10:57 -0700

    add testing and question to script

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Rob is the bomb');
+console.log("Let's place a game of tic tac toe!");
 
 const readline = require('readline');
 
@@ -7,17 +7,50 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Do you want to play Tic Tac Toe? (Y/N)', function(answer) {
+rl.question('Are you ready? (Y/N)', function(answer) {
+  var firstRow = ("      1|2|3");
+  var lineRow = ("      -----");
+  var secondRow = ("      4|5|6");
+  var thirdRow = ("      7|8|9");
+
   if (answer.toUpperCase() === "Y") {
-    console.log("Great! You go first!");
-    console.log('1|2|3');
-    console.log('-----');
-    console.log('4|5|6');
-    console.log('-----');
-    console.log('7|8|9');
+    console.log("Here is the board");
+    console.log(firstRow);
+    console.log(lineRow);
+    console.log(secondRow);
+    console.log(lineRow);
+    console.log(thirdRow);
+
   } else {
-    console.log('Party pooper!');
+    console.log('Fine...Bye Felicia (>_>)');
+    rl.close();
   }
 
-  rl.close();
+  rl.question("Player X goes first, choose a position: (1-9)", function(answer) {
+    if (parseInt(answer) === 5) {
+      secondRow = ("      4|X|6");
+      console.log(firstRow);
+      console.log(lineRow);
+      console.log(secondRow);
+      console.log(lineRow);
+      console.log(thirdRow);
+    }
+    else if (parseInt(answer) === 4) {
+      secondRow = ("      X|5|6");
+      console.log(firstRow);
+      console.log(lineRow);
+      console.log(secondRow);
+      console.log(lineRow);
+      console.log(thirdRow);
+    }
+    else if (parseInt(answer) === 1) {
+      firstRow = ("      X|2|3");
+      console.log(firstRow);
+      console.log(lineRow);
+      console.log(secondRow);
+      console.log(lineRow);
+      console.log(thirdRow);
+    }
+
+  });
 });
