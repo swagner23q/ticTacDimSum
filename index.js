683c18043011f101004a3b851c6df412f196ab9c
sara <sara@mash.is>
2016-06-08 14:29:53 -0700

    restart with three basic functions

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("Let's place a game of tic tac toe!");
-
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -7,51 +5,62 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Are you ready? (Y/N)', function(answer) {
-  var firstRow = ("      1|2|3");
-  var lineRow = ("      -----");
-  var secondRow = ("      4|5|6");
-  var thirdRow = ("      7|8|9");
+//global variables
+var board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-  if (answer.toUpperCase() === "Y") {
+//game functions
+var startGame = function() {
+  rl.question("Would you like to start? (Y/N)", function(answer) {
+    if (answer.toUpperCase() === "Y") {
+      console.log("Here is the board");
+      console.log(board);
 
-    console.log("Here is the board");
-    console.log(firstRow);
-    console.log(lineRow);
-    console.log(secondRow);
-    console.log(lineRow);
-    console.log(thirdRow);
-
-  } else {
-    console.log('Fine...Bye Felicia (>_>)');
-    rl.close();
-  }
-
-  rl.question("Player X goes first, choose a position: (1-9)", function(answer) {
-    if (parseInt(answer) === 5) {
-      secondRow = ("      4|X|6");
-      console.log(firstRow);
-      console.log(lineRow);
-      console.log(secondRow);
-      console.log(lineRow);
-      console.log(thirdRow);
-    }
-    else if (parseInt(answer) === 4) {
-      secondRow = ("      X|5|6");
-      console.log(firstRow);
-      console.log(lineRow);
-      console.log(secondRow);
-      console.log(lineRow);
-      console.log(thirdRow);
-    }
-    else if (parseInt(answer) === 1) {
-      firstRow = ("      X|2|3");
-      console.log(firstRow);
-      console.log(lineRow);
-      console.log(secondRow);
-      console.log(lineRow);
-      console.log(thirdRow);
+    } else {
+      console.log('Fine...Bye Felicia (>_>)');
+      rl.close();
     }
 
   });
-});
+}
+var replaceNumberX = function(userAnswer) {
+  if (userAnswer === board[userAnswer]) {
+    return board[userAnswer] = "X";
+  }
+}
+var replaceNumberO = function(userAnswer) {
+  if (userAnswer === board[userAnswer]) {
+    return board[userAnswer] = "O";
+  }
+}
+
+//initiate game
+
+console.log("Welcome to Tic Tac Toe");
+startGame();
+
+
+
+// var startGame = function() {
+//   rl.question('Are you ready? (Y/N)', function(answer) {
+//     var firstRow = ("      1|2|3");
+//     var lineRow = ("      -----");
+//     var secondRow = ("      4|5|6");
+//     var thirdRow = ("      7|8|9");
+//
+//     if (answer.toUpperCase() === "Y") {
+//
+//       console.log("Here is the board");
+//       console.log(firstRow);
+//       console.log(lineRow);
+//       console.log(secondRow);
+//       console.log(lineRow);
+//       console.log(thirdRow);
+//
+//     } else {
+//       console.log('Fine...Bye Felicia (>_>)');
+//       rl.close();
+//     }
+//
+//   });
+// }
+// startGame();
--- test/test.js
+++ test/test.js
@@ -4,7 +4,11 @@ var index = require('../index.js');
 describe('index', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-
     });
   });
+  describe('replaceNumber', function () {
+    it('should covert array board into a string', function () {
+      expect(index.board).to.equal("0, 1, 2, 3, 4, 5, 6");
+    });
+   });
 });
