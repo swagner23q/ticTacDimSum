33ee7382d5e95d02a1026525b4eadda991168c85
sara <sara@mash.is>
2016-06-08 16:01:53 -0700

    enable player turns with replace function working

--- index.js
+++ index.js
@@ -7,37 +7,65 @@ const rl = readline.createInterface({
 
 //global variables
 var board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var lines = "-----------";
 
 //game functions
 var startGame = function() {
   rl.question("Would you like to start? (Y/N)", function(answer) {
     if (answer.toUpperCase() === "Y") {
       console.log("Here is the board");
-      console.log(board);
+      console.log(board.slice(0, 3));
+      console.log(board.slice(3, 6));
+      console.log(board.slice(6, 9));
+      playerTurnX();
+
 
     } else {
       console.log('Fine...Bye Felicia (>_>)');
       rl.close();
     }
-
   });
 }
-var replaceNumberX = function(userAnswer) {
-  if (userAnswer === board[userAnswer]) {
-    return board[userAnswer] = "X";
+
+var playerTurnX = function() {
+  rl.question("Player X's move, choose a position (0-8)", function(answer) {
+    replaceNumberX(parseInt(answer));
+    console.log(board.slice(0, 3));
+    console.log(board.slice(3, 6));
+    console.log(board.slice(6, 9));
+    playerTurnO();
+  });
+}
+var playerTurnO = function() {
+  rl.question("Player O's move, choose a position (0-8)", function(answer) {
+    replaceNumberO(parseInt(answer));
+    console.log(board.slice(0, 3));
+    console.log(board.slice(3, 6));
+    console.log(board.slice(6, 9));
+    playerTurnX();
+  });
+}
+
+var replaceNumberX = function(answer) {
+  if (answer === board[answer]) {
+    board[answer] = "X";
   }
 }
-var replaceNumberO = function(userAnswer) {
-  if (userAnswer === board[userAnswer]) {
-    return board[userAnswer] = "O";
+var replaceNumberO = function(answer) {
+  if (answer === board[answer]) {
+    board[answer] = "O";
   }
+}
+
+var checkWinX = function(answer) {
+
+  
 }
 
 //initiate game
 
 console.log("Welcome to Tic Tac Toe");
 startGame();
-
 
 
 // var startGame = function() {
