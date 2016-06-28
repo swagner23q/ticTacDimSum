ebb62d9daad937c2c1bd67b384dc9ccc0684956f
Sara Wagner <sara@mash.is>
2016-06-28 13:55:29 -0700

    Add the game directory.

---
+++ www/src/js/game/webPresenter.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var webPresenter = function(gameState) {
+  this.gameState = gameState;
+  this.currentPlayer = 0;
+};
+
+webPresenter.prototype.setupBoard = function (boardSize) {
+  this.gameState.setupBoard(boardSize);
+}
+
+webPresenter.prototype.printBoard = function () {
+  var boardHtml = "<table>";
+  for (var row = 0; row < this.gameState.boardSize; row++) {
+    boardHtml += "<tr>";
+    for (var col = 0; col < this.gameState.boardSize; col++) {
+      boardHtml += "<td>" + this.gameState.board[row][col] + "</td>";
+    }
+    boardHtml += "</tr>";
+  }
+  boardHtml += "</table>";
+
+  return boardHtml;
+};
+
+webPresenter.prototype.setMove = function (row, col, value) {
+  this.gameState.setMove(row, col, value);
+  this.changeCurrentPlayer();
+};
+
+webPresenter.prototype.getBoard = function () {
+  return this.gameState.board;
+};
+
+webPresenter.prototype.getCurrentPlayer = function () {
+  return this.currentPlayer;
+};
+
+webPresenter.prototype.changeCurrentPlayer = function () {
+  if (this.currentPlayer === 0) {
+    this.currentPlayer = 1;
+  } else {
+    this.currentPlayer = 0;
+  }
+};
+
+var state = new gameState();
+var presenter = new webPresenter(state);
+
+
+//jQuery here
+$(function() {
+  presenter.setupBoard(3);
+
+  $('#gameBoard').html(presenter.printBoard())
+
+  $('#gameBoard td').each(function() {
+    $(this).click(function() {
+      var col = $(this).index();
+      var row = $(this).parent().index();
+
+      presenter.setMove(row, col, presenter.getCurrentPlayer());
+      $(this).html(presenter.getCurrentPlayer());
+      console.log(presenter.getBoard());
+
+    });
+  });
+
+
+
+});
+
+
+
+module.exports.webPresenter = webPresenter;
